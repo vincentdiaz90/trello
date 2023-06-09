2861fc50b2ab3abca112f65c05e45a39091e1978
vincentdiaz90 <62910374+vincentdiaz90@users.noreply.github.com>
2023-06-09 18:19:50 +0200

    fin css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,138 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="trello-container">
+      <nav className='navbar app'> App bar</nav>
+      <nav className='navbar board'> Board bar</nav>
+      <div className='board-columns'>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+          <li>third</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png" alt="entête design et recherche" />
+            Design & recherch
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        
+      </div>
+    </div>
   );
 }
 
